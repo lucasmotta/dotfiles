4a3de3451fb545967bbb7ed825e141015bfc386e
Lucas Motta <mail@lucasmotta.com>
2021-03-06 18:32:15 -0300

    Fix brew commands

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -7,7 +7,7 @@ title "Homebrew"
 
 # install homebrew
 if ! which brew > /dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # make sure we’re using the latest homebrew
@@ -27,30 +27,30 @@ brew install tree
 brew install watchman
 
 # install apps
-brew cask install 1password
-brew cask install android-studio
-brew cask install appcleaner
-brew cask install brave-browser
-brew cask install finicky
-brew cask install firefox
-brew cask install flipper
-brew cask install google-chrome
-brew cask install imageoptim
-brew cask install iterm2
-brew cask install kap
-brew cask install maccy
-brew cask install opera
-brew cask install phoenix
-brew cask install reactotron
-brew cask install sketch
-brew cask install slack
-brew cask install spotify
-brew cask install transmission
-brew cask install visual-studio-code
-brew cask install vlc
-brew cask install whatsapp
-brew cask install zeplin
-brew cask install zoomus
+brew install --cask 1password
+brew install --cask android-studio
+brew install --cask appcleaner
+brew install --cask brave-browser
+brew install --cask finicky
+brew install --cask firefox
+brew install --cask flipper
+brew install --cask google-chrome
+brew install --cask imageoptim
+brew install --cask iterm2
+brew install --cask kap
+brew install --cask maccy
+brew install --cask opera
+brew install --cask phoenix
+brew install --cask reactotron
+brew install --cask sketch
+brew install --cask slack
+brew install --cask spotify
+brew install --cask transmission
+brew install --cask visual-studio-code
+brew install --cask vlc
+brew install --cask whatsapp
+brew install --cask zeplin
+brew install --cask zoomus
 
 # install appstore apps
 mas 937984704       # Amphetamine
