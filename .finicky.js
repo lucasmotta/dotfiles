b439d856bbb93002db74d209574dd4323348a4a7
Lucas Motta <mail@lucasmotta.com>
2025-05-13 12:53:10 -0300

    heylucas

--- .finicky.js
+++ .finicky.js
@@ -1,13 +1,52 @@
 module.exports = {
-  defaultBrowser: 'Safari',
+  defaultBrowser: 'Arc',
   handlers: [
+    // Open localhost on Firefox
     {
-      match: finicky.matchHostnames(['bitbucket.org', 'atlassian.net']),
-      browser: 'Brave Browser',
+      match: finicky.matchDomains('localhost'),
+      browser: 'Firefox',
     },
+    // Open Zoom links directly in the app
     {
-      match: ({ url }) => url.host.includes('modulz'),
-      browser: 'Brave Browser',
+      match: ['zoom.us/*', finicky.matchDomains(/.*\zoom.us/), /zoom.us\/j\//],
+      browser: 'us.zoom.xos',
+    },
+    // Open workos on Firefox
+    {
+      match: /workos/,
+      browser: 'Firefox',
+    },
+    // Figma
+    {
+      match: 'https://www.figma.com/file/*',
+      browser: 'Figma',
+    },
+  ],
+  rewrite: [
+    // Remove marketing tags from url
+    // https://github.com/johnste/finicky/wiki/Configuration-ideas#remove-all-marketingtracking-information-from-urls
+    {
+      match: () => true,
+      url: ({ url }) => {
+        const removeKeysStartingWith = ['utm_', 'uta_', 'matt_']
+        const removeKeys = ['fbclid', 'gclid']
+
+        const search = url.search
+          .split('&')
+          .map((parameter) => parameter.split('='))
+          .filter(
+            ([key]) =>
+              !removeKeysStartingWith.some((startingWith) =>
+                key.startsWith(startingWith)
+              )
+          )
+          .filter(([key]) => !removeKeys.some((removeKey) => key === removeKey))
+
+        return {
+          ...url,
+          search: search.map((parameter) => parameter.join('=')).join('&'),
+        }
+      },
     },
   ],
 }
